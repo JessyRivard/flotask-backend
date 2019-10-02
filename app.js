219018b3e3c36c1c44daff6bc8d264cc7fc21693
Unknown <jessyrivard@icloud.com>
2019-10-02 10:50:42 -0400

    Coors Policy Added
    Port also changed to 3001 for testing

--- app.js
+++ app.js
@@ -12,6 +12,12 @@ var usersRouter = require("./routes/users");
 
 var app = express();
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+})
+
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -20,7 +26,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/api/lists", listsRouter);
 app.use("/api/tasks", tasksRouter);
-app.use("/users", usersRouter);
+app.use("/api/users", usersRouter);
 
 // connect to mongoDB and monitor connection
 try {
